d1d4d2daaab41fbfb8917b37d9081d3de6c155c8
Shivam Malhotra <malhotrashivam77@gmail.com>
2023-07-10 17:08:06 +0530

    update

--- ScriptSC.js
+++ ScriptSC.js
@@ -4,3 +4,26 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
   });
+
+
+
+  // -------contact----
+
+  const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
